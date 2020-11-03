ad56f136735cbbf1312e59e17cf6d926537a12a7
Roger Coverzola <diolink.4103@gmail.com>
2020-11-03 14:15:31 -0300

    challenge pages layout

--- src/pages/challenge/[id].js
+++ src/pages/challenge/[id].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Paper } from '@material-ui/core';
 import { listFiles } from '../../utils/files';
 import Link from 'next/link';
 
@@ -12,12 +12,19 @@ const styles = makeStyles({
     display: 'flex',
     flex: 1,
     minHeight: '100vh',
+    overflow: 'hidden',
   },
   title: {
-    margin: 5,
+    margin: '26px 0',
     color: '#fff',
     textShadow: '-7px 8px 8px black',
     cursor: 'pointer',
+  },
+  containerCode: {
+    padding: 12,
+    display: 'flex',
+    flex: 1,
+    paddingBottom: '60vh',
   },
 });
 
@@ -36,6 +43,18 @@ const challen = ({ file }) => {
             {file}
           </Typography>
         </Link>
+        <Grid container direction="row" justify="space-evenly" spacing={3}>
+          <Grid item xs={12} md={4}>
+            <Paper elevation={2} className={classes.containerCode}>
+              codigo
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper elevation={2} className={classes.containerCode}>
+              resultados
+            </Paper>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
